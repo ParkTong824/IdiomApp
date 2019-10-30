403ecda1444a5971b62968fa81c0ab4a75348c09
parkjunghun <go9018@naver.com>
2019-10-30 14:39:01 +0900

    add : bottomNavigation & two fragment

--- app/src/main/java/com/example/idiom/MainActivity.java
+++ app/src/main/java/com/example/idiom/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.idiom;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.idiom.ui.HomeFragment;
+import com.example.idiom.ui.StudyFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    FrameLayout containerView;
+    HomeFragment homeFragment;
+    StudyFragment studyFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.first_bottom :
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container_frame, homeFragment).commit();
+                        break;
+                    case R.id.second_bottom :
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container_frame, studyFragment).commit();
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void initView(){
+        bottomNavigationView = findViewById(R.id.bottomNav);
+        containerView = findViewById(R.id.container_frame);
+        homeFragment = new HomeFragment();
+        studyFragment = new StudyFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container_frame, homeFragment).commit();
     }
 }
---
+++ app/src/main/java/com/example/idiom/ui/StudyFragment.java
@@ -0,0 +1,28 @@
+package com.example.idiom.ui;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.idiom.R;
+
+public class StudyFragment extends Fragment {
+
+
+    public StudyFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_study, container, false);
+    }
+
+}
